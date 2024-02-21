66cae3b2c2d86b8c45181f39bc4f3aeaf21e9ef9
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-02-21 23:32:04 +0500

    add a test file

---
+++ test.js
@@ -0,0 +1,80 @@
+// test start date 21/ 02 / 2024 ; 10:53pm
+
+// bismillah
+
+//1 alert
+alert('my first js test');
+
+// 2 var with string
+
+var name = 'hussain' ;
+var pc = 'dell';
+
+console.log(name);
+console.log(pc);
+
+// 2 var with Number
+
+var num1 = 1;
+var num2 = 2;
+console.log(num1,num2);
+
+// 4 variable legal and illegal
+
+var camilCase = 'good prec';
+
+// 5 math experassions fimiliar operator
+
+var balance = 20;
+var bonus = 3;
+
+var sum = balance + bonus;
+var sum1 = balance - bonus;
+var sum2 = balance * bonus;
+var sum3 = balance / bonus;
+
+console.log(sum, sum1, sum2, sum3);
+
+// 6 math experassion  un fimiliar operator
+
+var blue = 2;
+var red = 3;
+var green = 5;
+
+var mix = green - blue + red; 
+var mxi = blue * red - green; 
+
+console.log(mix);
+console.log(mxi);
+
+// 7 concatenating text string
+
+var resever = 'ali';
+var order = 'pizza ';
+
+alert('Hi '+ resever + ' your order is ' + order);
+
+// 8 eliminating algiminaty
+
+var mathIO = (2 + 3);
+var mathUi = ((7+9) * 3) ;
+var mathTy = (7+9) + (88); 
+var mathOp = (7+9 - (10) + (2)) ;
+
+console.log(mathIO + ' ' + mathOp + ' ' +mathTy+' ' +mathUi );
+
+// 09 prompt 
+
+var userInput = prompt('your name');
+var ageInput = prompt('your age');
+
+console.log(userInput + ' ' + ageInput);
+
+// 10 if statment
+
+var project = prompt('sucsses');
+if('sucsses'){
+console.log('good');
+}
+
+
