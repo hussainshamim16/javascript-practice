c275abff6bd02768fd05ccb0c57bdb174dec2738
MUHAMMAD HUSSAIN SHAMIM <hj422748@gmail.com>
2024-06-07 08:29:48 +0500

    while loop

--- test.js
+++ test.js
@@ -177,3 +177,14 @@ if(perra.slice(i,i+6) === "Typing"){
 }
 }
 // console.log(perra)
+
+/// while loop 
+console.log("whlie Loop");
+var i = 0;
+while(i<5){
+    console.log(i)
+
+    i++
+}
+// for (var i= 0; i<=5; ){
+// }
