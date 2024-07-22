145c9580afb9a20c35a6f06c7e413ce387b9aa0d
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-07-22 13:04:22 +0500

    js map set

---
+++ map/script.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+
+var userData= [
+    "hussain",
+    "ali",
+    "kaleem"
+];
+
+userData.map((index,value,array)=>{
+    console.log(index);
+    console.log(value);
+    console.log(array);
+});
+
+
---
+++ set/script.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+
+var array = new Set([
+    "hussain",
+    "hussain",
+    "hussain",
+    "shamim",
+    "shamim",
+    "shamim",
+    "aleem",
+    "aleem",
+    "aleem",
+    "aleem"
+]);
+
+console.log(array);
+
+// jetni bh9i values dallo vo aike hei display ho gee bs 
+
+
