3a8a11a3282347191e603b95052b0889c2382b72
hussainshamim16 <hj422748@gmail.com>
2025-11-22 09:53:21 +0500

    JAVASCRIPT REVISION

---
+++ Javascript Rev/server.js
@@ -0,0 +1,43 @@
+console.log("Hello World starting at office 11/22/2025 - 8:00 Am");
+
+// 1 ALert
+// Alert js ka aike built-in popup box hey jo user ko message dikhaney key liye use hota hey
+// alert("Sallam Duniya");
+
+// 2 Variables 
+// data store karney vala aike box hota hey jes mey ham diffrent diffrent type ka data rakh sakty hen 
+var developerName = "Hussain Shamim";
+
+// 3 Illegal variable Names 
+// Aise variable names jo JavaScript accept nahi karta
+// like vo start number sey hon 2 space sey start hun 3 js ka koi keyword hon 4 spacial characters hon
+
+// 4 Math familiar operator
+// vo opertors jo basic maths mey use hoty hen addison subtraction multiplication division remainder
+
+// 5 Math UnFamiliar Operators
+// vo operators jo Maths mey nhi hen but js onhey use karti hey
+// ++ --   X++ pehly use baad mey increase ++X pehly increase phir use
+
+// 6 Math expressions – Eliminating ambiguity
+// jab expression mey multiple operators hothey like familiar operators to js PEMDAS role follow karta hey 
+let result = 10 + 5 * 2;   // 20 (5*2 pehle)
+let result2 = (10 + 5) * 2; // 30 (parentheses pehle)
+
+// 7 Propmt 
+// user sey input leta hey aike popup open kar key input string mey ati hey 
+
+// 8 If Statement
+// javascript mey condition check karney key liye hota hey 
+if(9 > 2){
+    console.log("yes 9 > 2")
+}
+
+// 9 comparision Operators
+// js mey value ko comapre karney key liye use hoty hen 
+
+// 10 else if
+// agger multiple conditions check karnei ho to else if lagaty hen 
+
+// 11 logic Operators
+// and ___ OR
